9d96a9f64451ebb71a40d1b11c72ea9350da8522
cdake <daniyarboran@narod.ru>
2022-08-23 17:26:26 +0600

    feat: changes to routes

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const sequelize = require('./database')
-const User = require('./models/user')
-const Promocodes = require('./models/promocodes')
+// const User = require('./models/user')
+// const Promocodes = require('./models/promocodes')
 const port = process.env.PORT
+const userRouter = require('./routes/users')
 
 //Создаем маблицы в БД
 sequelize.sync()
 
 sequelize.sync({force: true})
 
-
-const users = require('./models/user')
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
@@ -22,12 +21,12 @@ app.set('static', __dirname + 'static')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-// app.use('/users', userRouter)
+app.use('/users', userRouter)
 
 
 app.get('/', async (req, res)=>{
-    const jane = await User.create({ first_name: "Jane", last_name: "Doe", iin: "123456789123" });
-    res.send(jane)
+    // const jane = await User.create({ first_name: "Jane", last_name: "Doe", iin: "123456789123" });
+    res.send('jane')
 })
 
 
