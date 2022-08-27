f12a0d386ced783ab5d2958ff418e4d74730343d
cdake <daniyarboran@narod.ru>
2022-08-27 10:27:02 +0600

    feat: change Users route add raw SQL

--- models/user.js
+++ models/user.js
@@ -1,57 +1,15 @@
-const Sequelize = require('sequelize')
+const express = require('express');
+const router = express.Router();
 
-const sequelize = require('../database')
 
-const User = sequelize.define('user', {
-    user_id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    
-    first_name: {
-        type: Sequelize.STRING, 
-        allowNull: false
-    },
-
-    last_name: {
-        type: Sequelize.STRING, 
-        allowNull: false
-    },
-
-    iin: {
-        type: Sequelize.STRING(12), 
-        allowNull: false
-    },
-
-    phone_no: {
-        type: Sequelize.STRING(15),
-        allowNull: false,
-    },
-
-    picture: {
-        type:Sequelize.STRING,
-        defaultValue: 'static/img/avatar.png'
-    },
-
-    is_active: {
-        type:Sequelize.BOOLEAN,
-        defaultValue: false
-    },
-
-    created_at: {
-        type: Sequelize.DATE,
-    },
-    
-    promo_id_fk:{
-        type: Sequelize.INTEGER,
-        references :{
-            model: 'promocodes',
-            key: 'code_id'
-        }
-    }},
-    {timestamps: false}
-    )
-
-module.exports = User
+router.get('/', (req, res)=>{
+    let sql="SELECT * FROM users";
+    db.query(sql, (err,data, fields)=>{
+        if (err) throw err;
+        res.json({
+            status: 200,
+            data,
+            message: "User list retrieved"
+        })
+    })
+})
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,51 @@
-
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user')
 
-router.get('/list',async (req, res)=>{
-    const users = await User.findAll();
-    res.json({ users, message: "User list received successfully"}) 
-});
 
-router.post('/new', async (req,res)=>{
-    const {first_name, last_name, iin, promocode_id_fk, phone_no} = req.body
+router.get('/', (req, res)=>{
+    let sql="SELECT * FROM users";
+    db.query(sql, (err,data, fields)=>{
+        if (err) throw err;
+        res.json({
+            status: 200,
+            data,
+            message: "User list retrieved"
+        })
+    })
+})
+
+router.post('/signup', async (req,res)=>{
+    const {first_name, last_name, iin, phone_no} = req.body
     const created = new Date()
-    if(!first_name || !last_name || !iin) {
+    if(!phone_no){
+        res.status(400).send("Provide phone number")
+    }
+    
+    if (!first_name || !last_name || !iin || !phone_no) {
         res.status(400).send("Bad Request")
-    } else {
-    const newUser = await User.create({first_name:first_name, last_name:last_name, iin:iin, created_at:created, phone_no:phone_no, promo_id_fk:promocode_id_fk})
-    res.json({status: 200, message:"User created", newUser})}
+    }
+    if (phone_no.match(/^(?:[+0]7)?[0-9]{10}$/g)) {
+        let sql="SELECT * FROM users WHERE phone_no = ?"
+        db.query(sql,[phone_no],(err, data, fields)=>{
+            if (err) throw err;
+            console.log(data)
+        if(data==""){
+            let sql = `INSERT INTO users(first_name, last_name, iin, phone_no, created_at) VALUES(?,?,?,?,?)`
+            db.query(sql,[first_name, last_name, iin, phone_no, created], function(err, data, fields){
+                if (err) throw err
+                console.log("--data ", data)
+                res.json({
+                    status:200,
+                    message:"User added"
+                })
+        })
+        } else {
+            res.json({
+                message:"User already exist"
+            })
+        }
+        })
+    }
 })
 
 module.exports = router;
